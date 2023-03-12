ac6d1f48ef3b330ead80535520582a0051363f2f
Mustafa SOLAK <mustafasolak48@gmail.com>
2023-03-12 23:59:56 +0300

    Bootstrap Entegrasyonu ve Pagination
    Bootstrap Entegrasyonu ve Pagination

---
+++ project_18_bootstrap_entegrasyonu/resources/views/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+@section('baslik')
+    Bootstrap Entegrasyonu
+@endsection
+
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Adı Soyadı</th>
+                <th scope="col">Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <div class="row">
+        {!! $users->links() !!}
+    </div>
+</div>
+
+ 
---
+++ project_18_bootstrap_entegrasyonu/resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('baslik')</title>
+    <link href="{{ asset("front/css/bootstrap.min.css") }}" rel="stylesheet" >
+  </head>
+  <body>
+ 
+    <script src="{{ asset("front/js/bootstrap.min.js") }}" ></script>
+  </body>
+</html>
